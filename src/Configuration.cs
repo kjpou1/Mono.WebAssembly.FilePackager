66039d1214f394f834fbd3c4875ea40c0ec0931f
kjpou1 <kjpou@pt.lu>
2019-11-21 10:31:32 +0100

    Add solution

---
+++ src/Configuration.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mono.WebAssembly.FilePackager
+{
+    public class Configuration
+    { }
+}
---
+++ src/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Mono.Options;
+
+namespace Mono.WebAssembly.FilePackager
+{
+    class Program
+    {
+        static Configuration config = new Configuration();
+        static void Main(string[] args)
+        {
+            var shouldShowHelp = false;
+            var options = new OptionSet {
+                { "h|help", "show this message and exit", h => shouldShowHelp = h != null },
+            };
+
+            List<string> extra;
+            try
+            {
+                extra = options.Parse(args);
+            }
+            catch (OptionException e)
+            {
+                // output some error message
+                Console.Write("Mono.WebAssembly.FilePackger.exe: ");
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Try `Mono.WebAssembly.FilePackger.exe --help' for more information.");
+                return;
+            }
+
+        }
+    }
+}
