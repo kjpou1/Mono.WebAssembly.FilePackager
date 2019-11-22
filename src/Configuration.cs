a6953abf8b6d04db027c767b630b72bb473042e2
Kenneth Pouncey <kjpou@pt.lu>
2019-11-22 08:58:01 +0100

    Add options parsing

--- src/Configuration.cs
+++ src/Configuration.cs
@@ -5,5 +5,12 @@ using System.Text;
 namespace Mono.WebAssembly.FilePackager
 {
     public class Configuration
-    { }
+    {
+        public string Target { get; set; }
+        public string PreloadFile { get; set; }
+        public string JSOutput { get; set; }
+        public bool HeapCopy { get; set; } = true;
+        public bool SeparateMetaData { get; set; } = false;
+
+    }
 }
--- src/Program.cs
+++ src/Program.cs
@@ -7,10 +7,30 @@ namespace Mono.WebAssembly.FilePackager
     class Program
     {
         static Configuration config = new Configuration();
+        static OptionSet options;
+
         static void Main(string[] args)
         {
             var shouldShowHelp = false;
-            var options = new OptionSet {
+            const string preloadfile = @"preload-file <filename>.  Specify a file to preload before running the compiled code
+asynchronously.  The path is relative to the current directory at compile time. If a directory is passed here, its entire contents
+will be embedded.
+
+Preloaded files are stored in `<filename>.data`, where `<filename>.html` is the main file you are compiling to.
+To run your code, you will need both the `<filename>.html` and the `<filename>.data`.";
+
+            const string noHeapCopy = @"If specified, the preloaded filesystem is not copied inside the Emscripten HEAP, but kept in a separate typed array outside it.  
+The default, if this is not specified, is to embed the VFS inside the HEAP, so that mmap()ing files in it is a no-op.
+Passing this flag optimizes for fread() usage, omitting it optimizes for mmap() usage.";
+
+            const string separateMetadata = @"Stores package metadata separately. Only applicable when preloading and js-output file is specified.";
+
+            options = new OptionSet {
+                { "t|target=", "target data filename." , t => config.Target = t },
+                { "p|preload-file=", preloadfile , plf => config.PreloadFile = plf },
+                { "j|js-output=", "Writes output in FILE, if not specified, standard output is used." , jso => config.JSOutput = jso },
+                { "n|no-heap-copy", noHeapCopy , _ => config.HeapCopy = false },
+                { "s|separate-metadata", separateMetadata , _ => config.SeparateMetaData = true },
                 { "h|help", "show this message and exit", h => shouldShowHelp = h != null },
             };
 
@@ -28,6 +48,32 @@ namespace Mono.WebAssembly.FilePackager
                 return;
             }
 
+            if (string.IsNullOrEmpty(config.Target))
+            {
+                shouldShowHelp = true;
+
+            }
+
+            if (shouldShowHelp)
+            {
+                Usage();
+            }
+
+            Console.WriteLine($"Target: {config.Target}");
+
+
+        }
+
+        static void Usage()
+        {
+            // show some app description message
+            Console.WriteLine("Usage: Mono.WebAssembly.FilePackger.exe --target TARGET [--preload A [B..]] [--js-output=OUTPUT.js] [--no-heap-copy] [--separate-metadata]");
+            Console.WriteLine();
+
+            // output the options
+            Console.WriteLine("Options:");
+            options.WriteOptionDescriptions(Console.Out);
         }
     }
+
 }
