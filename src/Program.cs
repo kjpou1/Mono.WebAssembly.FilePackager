dd6b2e29ad3badcdc2dd3626c3e99492a6115cdf
Marcos Cobeña Morián <mcobena@plainconcepts.com>
2019-12-02 13:12:48 +0100

    Initial bunch of code translated (still not tested)

--- src/Program.cs
+++ src/Program.cs
@@ -1,13 +1,25 @@
-﻿using System;
+﻿/* Stuff considered while porting this file from Python:
+ * - Var names are left equal, except been C-Sharpified: this way will help us keep changes from the original source
+ */
+
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using Mono.Options;
 
 namespace Mono.WebAssembly.FilePackager
 {
     class Program
     {
+        const string ExportName = "Module";
+
         static Configuration config = new Configuration();
         static OptionSet options;
+        static List<(string srcPath, string dstPath, string mode, bool explicitDstPath)> newDataFiles = 
+            new List<(string srcPath, string dstPath, string mode, bool explicitDstPath)>();
+        static List<string> newDirNames = new List<string>();
 
         static void Main(string[] args)
         {
@@ -61,7 +73,122 @@ Passing this flag optimizes for fread() usage, omitting it optimizes for mmap() 
 
             Console.WriteLine($"Target: {config.Target}");
 
+            Run();
+        }
 
+        static void Run()
+        {
+            var mode = "leading";
+            var preloadFile = new string(config.PreloadFile);
+            var atPosition = preloadFile.Replace("@@", "__").IndexOf('@');
+            var usesAtNotation = atPosition != -1;
+            string srcPath, dstPath;
+            var dataFiles = new List<(string srcPath, string dstPath, string mode, bool explicitDstPath)>();
+
+            if (usesAtNotation)
+            {
+                srcPath = preloadFile.Substring(0, atPosition).Replace("@@", "@");
+                dstPath = preloadFile.Substring(atPosition + 1).Replace("@@", "@");
+            }
+            else
+            {
+                srcPath = dstPath = preloadFile.Replace("@@", "@");
+
+                if (File.Exists(srcPath) || Directory.Exists(srcPath))
+                {
+                    dataFiles.Add((srcPath, dstPath, mode, usesAtNotation));
+                }
+                else
+                {
+                    Console.WriteLine($"Warning: {preloadFile} does not exist, ignoring.");
+                }
+            }
+
+            var ret = @$"
+var Module = typeof {ExportName} !== 'undefined' ? {ExportName} : {{}};
+";
+            ret += @"
+if (!Module.expectedDataFileDownloads)
+{
+  Module.expectedDataFileDownloads = 0;
+  Module.finishedDataFileDownloads = 0;
+}
+Module.expectedDataFileDownloads++;
+(function() {
+ var loadPackage = function(metadata) {
+";
+            var code = @"
+function assert(check, msg) {
+  if (!check) throw msg + new Error().stack;
+}
+";
+            foreach (var file in dataFiles)
+            {
+                if (!ShouldIgnore(file.srcPath))
+                {
+                    if (Directory.Exists(file.srcPath))
+                    {
+                        Add(file.mode, file.srcPath, file.dstPath);
+                    }
+                    else
+                    {
+                        newDataFiles.Add(file);
+                    }
+                }
+            }
+
+            dataFiles = newDataFiles.Where(file => !Directory.Exists(file.srcPath)).ToList();
+
+            if (dataFiles.Count == 0)
+            {
+                Console.WriteLine("Nothing to do!");
+                Environment.Exit(1);
+            }
+
+            // Continue at: https://github.com/emscripten-core/emscripten/blob/incoming/tools/file_packager.py#L310
+        }
+
+        static void Add(string mode, string rootpathsrc, string rootpathdst)
+        {
+            foreach (var fullName in Directory.EnumerateDirectories(rootpathsrc))
+            {
+                if (!ShouldIgnore(fullName))
+                {
+                    var name = Path.GetRelativePath(rootpathsrc, fullName);
+                    newDirNames.Add(name);
+                }
+                else
+                {
+                    Debug.WriteLine(
+                        $"Skipping directory \"{fullName}\" from inclusion in the emscripten virtual file system.");
+                }
+            }
+
+            foreach (var fullName in Directory.EnumerateFiles(rootpathsrc))
+            {
+                if (!ShouldIgnore(fullName))
+                {
+                    var dstPath = Path.Join(rootpathdst, Path.GetRelativePath(rootpathsrc, fullName));
+                    newDataFiles.Add((fullName, dstPath, mode, true));
+                }
+                else
+                {
+                    Debug.WriteLine($"Skipping file \"{fullName}\" from inclusion in the emscripten " +
+                        "virtual file system.");
+                }
+            }
+        }
+
+        static bool ShouldIgnore(string fullName)
+        {
+            var attributes = File.GetAttributes(fullName);
+
+            if (attributes.HasFlag(FileAttributes.Hidden))
+            {
+                return true;
+            }
+
+            return false;
         }
 
         static void Usage()
@@ -75,5 +202,4 @@ Passing this flag optimizes for fread() usage, omitting it optimizes for mmap() 
             options.WriteOptionDescriptions(Console.Out);
         }
     }
-
 }
